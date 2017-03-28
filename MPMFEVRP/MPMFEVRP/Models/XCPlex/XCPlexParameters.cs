6603c8b93dc4a722b15e9fc2b0e25125bb77704e
mtyavuzeus <myavuz@cba.ua.edu>
2017-03-28 15:04:21 -0500

    custSet added to XCPlexParameters

--- MPMFEVRP/MPMFEVRP/Models/XCPlex/XCPlexParameters.cs
+++ MPMFEVRP/MPMFEVRP/Models/XCPlex/XCPlexParameters.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using MPMFEVRP.Domains.AlgorithmDomain;
+using MPMFEVRP.Domains.SolutionDomain;
 
 namespace MPMFEVRP.Models.XCPlex
 {
@@ -8,12 +9,15 @@ namespace MPMFEVRP.Models.XCPlex
         double errorTolerance; public double ErrorTolerance { get { return errorTolerance; } }
         bool limitComputationTime; public bool LimitComputationTime { get { return limitComputationTime; } }
         XCPlexRelaxation relaxation; public XCPlexRelaxation Relaxation { get { return relaxation; } }
+        CustomerSet custSet; public CustomerSet CustSet { get { return custSet; } } public bool TSP { get { return (custSet != null); } }
+        //the additionals, for whatever need they may serve:
         Dictionary<ParameterID, Parameter> optionalCPlexParameters; public Dictionary<ParameterID, Parameter> OptionalCPlexParameters { get { return optionalCPlexParameters; } }
 
         public XCPlexParameters(
             double errorTolerance = 0.00001,
             bool limitComputationTime = false,
             XCPlexRelaxation relaxation = XCPlexRelaxation.None,
+            CustomerSet custSet = null,
             Dictionary<ParameterID, Parameter> optionalCPlexParameters = null
             )
         {
@@ -21,6 +25,7 @@ namespace MPMFEVRP.Models.XCPlex
             this.limitComputationTime = limitComputationTime;
             this.relaxation = relaxation;
             this.optionalCPlexParameters = optionalCPlexParameters;
+            this.custSet = custSet;
             if (optionalCPlexParameters == null)
                 this.optionalCPlexParameters = new Dictionary<ParameterID, Parameter>();
         }
