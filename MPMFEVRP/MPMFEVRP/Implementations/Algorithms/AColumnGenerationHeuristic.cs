7979707b6a43f17e07d078b281cc8ffe4c2281b8
mtyavuzeus <myavuz@cba.ua.edu>
2017-04-11 14:31:04 -0500

    AColumnGenerationHeuristic algorithm created

---
+++ MPMFEVRP/MPMFEVRP/Implementations/Algorithms/AColumnGenerationHeuristic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPMFEVRP.Interfaces;
+
+namespace MPMFEVRP.Implementations.Algorithms
+{
+    public class AColumnGenerationHeuristic : AlgorithmBase
+    {
+
+
+        public override string GetName()
+        {
+            return "Customer Set-based Column Generation Heuristic";
+        }
+
+        public override void SpecializedConclude()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SpecializedInitialize(ProblemModelBase model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SpecializedReset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SpecializedRun()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
