96621f1b713ad75e91de9afd8bf507eddc7294a5
Mesut Yavuz <myavuz@cba.ua.edu>
2017-07-31 16:37:47 -0500

    Initial work on Serialization to save the CS repository for Set Cover use later

---
+++ MPMFEVRP/MPMFEVRP/Utils/SerializationUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace MPMFEVRP.Utils
+{
+    public class SerializationUtil
+    {
+        public static void WriteToXmlFile<T>(string filePath, T objectToWrite, bool append = false) where T : new()
+        {
+            TextWriter writer = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                writer = new StreamWriter(filePath, append);
+                serializer.Serialize(writer, objectToWrite);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+    }
+}
