cb86d395c19b24461acbe260f4716306b5e7143c
Huseyin Ergin <mersinus@gmail.com>
2017-10-11 23:30:51 -0400

    Ignored files are deleted. Any cpus are removed. Nuget packages are ignored and removed.

---
+++ MPMFEVRP/MPMFEVRPTests1/Utils/DeepCopierTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPMFEVRP.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPMFEVRP.Utils.Tests
+{
+    [TestClass()]
+    public class DeepCopierTests
+    {
+        [TestMethod()]
+        public void CopyTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
