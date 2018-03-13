08d10ed4a8fd21eafa3424d65ead6beeb141c9f0
isilkoyuncu <ikoyuncu@crimson.ua.edu>
2018-03-12 21:27:01 -0500

    TS runs form full blown experiment part is finished.

--- MPMFEVRP/MPMFEVRP/Forms/TS_Runs.Designer.cs
+++ MPMFEVRP/MPMFEVRP/Forms/TS_Runs.Designer.cs
@@ -32,36 +32,31 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label11 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_solveEMHwithNDF = new System.Windows.Forms.Button();
+            this.button_solveYCwithNDF = new System.Windows.Forms.Button();
+            this.button_solveEMHwithADF = new System.Windows.Forms.Button();
+            this.button_solveYCwithADF = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 25);
+            this.label1.Location = new System.Drawing.Point(17, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 17);
+            this.label1.Size = new System.Drawing.Size(125, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "1- Full Blown Comparison";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 52);
+            this.label2.Location = new System.Drawing.Point(38, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(650, 17);
+            this.label2.Size = new System.Drawing.Size(493, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "In this experiment, we will read EMH and YC instances and then solve them with AD" +
     "F and NDF models.";
@@ -69,164 +64,100 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(95, 99);
+            this.label3.Location = new System.Drawing.Point(71, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(127, 17);
+            this.label3.Size = new System.Drawing.Size(421, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Number of EVs = 6";
+            this.label3.Text = "Min VMT, Number of EVs = 6, Number of GDVs = 0, Lambda = 1, CPU Time = 3600 sec";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(70, 73);
+            this.label4.Location = new System.Drawing.Point(52, 59);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(321, 17);
+            this.label4.Size = new System.Drawing.Size(245, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Create Problem Model of EMH with Min Total VMT";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(241, 99);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(139, 17);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Number of GDVs = 0";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(399, 99);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(75, 17);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Lambda=1";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(399, 155);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(75, 17);
-            this.label7.TabIndex = 9;
-            this.label7.Text = "Lambda=1";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(241, 155);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(139, 17);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "Number of GDVs = 7";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(70, 129);
+            this.label9.Location = new System.Drawing.Point(52, 108);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(309, 17);
+            this.label9.Size = new System.Drawing.Size(233, 13);
             this.label9.TabIndex = 7;
-            this.label9.Text = "Create Problem Model of YC with Min Total VMT";
+            this.label9.Text = "Create Problem Model of YC with Min Total Cost";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(95, 155);
+            this.label10.Location = new System.Drawing.Point(71, 132);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(127, 17);
+            this.label10.Size = new System.Drawing.Size(419, 13);
             this.label10.TabIndex = 6;
-            this.label10.Text = "Number of EVs = 7";
+            this.label10.Text = "Min Cost, Number of EVs = 7, Number of GDVs = 7, Lambda = 1, CPU Time = 3600 sec\r" +
+    "\n";
             // 
-            // button1
+            // button_solveEMHwithNDF
             // 
-            this.button1.Location = new System.Drawing.Point(506, 82);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(238, 34);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Read and Create Problem Model";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button_CreateEMHProblemModels);
+            this.button_solveEMHwithNDF.Location = new System.Drawing.Point(518, 70);
+            this.button_solveEMHwithNDF.Name = "button_solveEMHwithNDF";
+            this.button_solveEMHwithNDF.Size = new System.Drawing.Size(131, 23);
+            this.button_solveEMHwithNDF.TabIndex = 8;
+            this.button_solveEMHwithNDF.Text = "Solve EMH with NDF";
+            this.button_solveEMHwithNDF.UseVisualStyleBackColor = true;
+            this.button_solveEMHwithNDF.Click += new System.EventHandler(this.Button_solveEMHwithNDF_FF_Click);
             // 
-            // button2
+            // button_solveYCwithNDF
             // 
-            this.button2.Location = new System.Drawing.Point(506, 138);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(238, 34);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Read and Create Problem Model";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button_CreateYCProblemModels);
+            this.button_solveYCwithNDF.Location = new System.Drawing.Point(518, 122);
+            this.button_solveYCwithNDF.Name = "button_solveYCwithNDF";
+            this.button_solveYCwithNDF.Size = new System.Drawing.Size(131, 23);
+            this.button_solveYCwithNDF.TabIndex = 9;
+            this.button_solveYCwithNDF.Text = "Solve YC with NDF";
+            this.button_solveYCwithNDF.UseVisualStyleBackColor = true;
+            this.button_solveYCwithNDF.Click += new System.EventHandler(this.Button_solveYCwithNDF_FF_Click);
             // 
-            // button3
+            // button_solveEMHwithADF
             // 
-            this.button3.Location = new System.Drawing.Point(792, 82);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(146, 34);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "Solve with ADF";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Button_SolveWithADF);
+            this.button_solveEMHwithADF.Location = new System.Drawing.Point(695, 70);
+            this.button_solveEMHwithADF.Name = "button_solveEMHwithADF";
+            this.button_solveEMHwithADF.Size = new System.Drawing.Size(131, 23);
+            this.button_solveEMHwithADF.TabIndex = 10;
+            this.button_solveEMHwithADF.Text = "Solve EMH with ADF";
+            this.button_solveEMHwithADF.UseVisualStyleBackColor = true;
+            this.button_solveEMHwithADF.Click += new System.EventHandler(this.Button_solveEMHwithADF_FF_Click);
             // 
-            // button4
+            // button_solveYCwithADF
             // 
-            this.button4.Location = new System.Drawing.Point(792, 138);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(146, 34);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "Solve with NDF";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.Button_SolveWithNDF);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(792, 191);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(146, 41);
-            this.button5.TabIndex = 14;
-            this.button5.Text = "Solve with both";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.Button_SolveWithBoth);
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(978, 56);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(69, 17);
-            this.label11.TabIndex = 15;
-            this.label11.Text = "Run Time";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(981, 87);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(66, 22);
-            this.textBox1.TabIndex = 16;
-            this.textBox1.Text = "3600";
+            this.button_solveYCwithADF.Location = new System.Drawing.Point(695, 122);
+            this.button_solveYCwithADF.Name = "button_solveYCwithADF";
+            this.button_solveYCwithADF.Size = new System.Drawing.Size(131, 23);
+            this.button_solveYCwithADF.TabIndex = 11;
+            this.button_solveYCwithADF.Text = "Solve YC with NDF";
+            this.button_solveYCwithADF.UseVisualStyleBackColor = true;
+            this.button_solveYCwithADF.Click += new System.EventHandler(this.Button_solveYCwithADF_FF_Click);
             // 
             // TS_Runs
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1091, 595);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label8);
+            this.ClientSize = new System.Drawing.Size(1122, 500);
+            this.Controls.Add(this.button_solveYCwithADF);
+            this.Controls.Add(this.button_solveEMHwithADF);
+            this.Controls.Add(this.button_solveYCwithNDF);
+            this.Controls.Add(this.button_solveEMHwithNDF);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "TS_Runs";
             this.Text = "TS_Runs";
             this.ResumeLayout(false);
@@ -240,18 +171,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_solveEMHwithNDF;
+        private System.Windows.Forms.Button button_solveYCwithNDF;
+        private System.Windows.Forms.Button button_solveEMHwithADF;
+        private System.Windows.Forms.Button button_solveYCwithADF;
     }
 }
--- MPMFEVRP/MPMFEVRP/Forms/TS_Runs.cs
+++ MPMFEVRP/MPMFEVRP/Forms/TS_Runs.cs
@@ -1,45 +1,189 @@
-﻿using System;
+﻿using MPMFEVRP.Interfaces;
+using MPMFEVRP.Utils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Instance_Generation.Forms;
+using MPMFEVRP.Implementations.Solutions.Writers;
+using MPMFEVRP.Implementations.Algorithms;
+using MPMFEVRP.Implementations.Problems;
+using MPMFEVRP.Implementations.ProblemModels;
+using MPMFEVRP.Domains.AlgorithmDomain;
+using MPMFEVRP.Domains.ProblemDomain;
+using MPMFEVRP.Domains.SolutionDomain;
+using MPMFEVRP.Implementations.ProblemModels.Interfaces_and_Bases;
+using MPMFEVRP.Implementations.Problems.Interfaces_and_Bases;
+using MPMFEVRP.Implementations.Solutions.Interfaces_and_Bases;
 
 namespace MPMFEVRP.Forms
 {
     public partial class TS_Runs : Form
     {
+        BindingList<IProblem> problems;
+        BindingList<EVvsGDV_ProblemModel> problemModels;
+        BindingList<ISolution> solutions;
+
+        IProblem theProblem;
+        EVvsGDV_ProblemModel theProblemModel;
+        Outsource2Cplex theAlgorithm;
+        ISolution theSolution;
+        IWriter writer;
+        Type TSPModelType;
+
         public TS_Runs()
         {
             InitializeComponent();
-        }
 
-        private void Button_CreateEMHProblemModels(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Button_CreateYCProblemModels(object sender, EventArgs e)
-        {
+            problems = new BindingList<IProblem>();
+            problemModels = new BindingList<EVvsGDV_ProblemModel>();
 
         }
 
-        private void Button_SolveWithADF(object sender, EventArgs e)
+        private void Button_solveEMHwithNDF_FF_Click(object sender, EventArgs e)
         {
+            CreateXcplexFormulation();
+            theAlgorithm.AlgorithmParameters.UpdateParameter(Models.ParameterID.ALG_XCPLEX_FORMULATION, XCPlex_Formulation.NodeDuplicatingwoU);
+
+            CreateEMHProblem();
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_RECHARGING_ASSUMPTION, RechargingDurationAndAllowableDepartureStatusFromES.Fixed_Full);
+
+            CreateProblemModels();
+
+            Run();
 
         }
-
-        private void Button_SolveWithNDF(object sender, EventArgs e)
+        private void Button_solveEMHwithADF_FF_Click(object sender, EventArgs e)
         {
+            CreateXcplexFormulation();
+            theAlgorithm.AlgorithmParameters.UpdateParameter(Models.ParameterID.ALG_XCPLEX_FORMULATION, XCPlex_Formulation.ArcDuplicatingwoU);
+
+            CreateEMHProblem();
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_RECHARGING_ASSUMPTION, RechargingDurationAndAllowableDepartureStatusFromES.Fixed_Full);
+
+            CreateProblemModels();
+
+            Run();
 
         }
-
-        private void Button_SolveWithBoth(object sender, EventArgs e)
+        private void Button_solveYCwithNDF_FF_Click(object sender, EventArgs e)
         {
+            CreateXcplexFormulation();
+            theAlgorithm.AlgorithmParameters.UpdateParameter(Models.ParameterID.ALG_XCPLEX_FORMULATION, XCPlex_Formulation.NodeDuplicatingwoU);
 
+            CreateYCProblem();
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_RECHARGING_ASSUMPTION, RechargingDurationAndAllowableDepartureStatusFromES.Fixed_Full);
+
+            CreateProblemModels();
+
+            Run();
+
+        }
+        private void Button_solveYCwithADF_FF_Click(object sender, EventArgs e)
+        {
+            CreateXcplexFormulation();
+            theAlgorithm.AlgorithmParameters.UpdateParameter(Models.ParameterID.ALG_XCPLEX_FORMULATION, XCPlex_Formulation.ArcDuplicatingwoU);
+
+            CreateEMHProblem();
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_RECHARGING_ASSUMPTION, RechargingDurationAndAllowableDepartureStatusFromES.Fixed_Full);
+
+            CreateProblemModels();
+
+            Run();
+        }
+
+        void Run()
+        {
+            solutions = new BindingList<ISolution>();
+            foreach (var problemModel in problemModels)
+            {
+                theAlgorithm.Initialize(problemModel);
+                theAlgorithm.Run();
+                theAlgorithm.Conclude();
+                theSolution = theAlgorithm.Solution;
+                solutions.Add(theSolution);
+                writer = new IndividualSolutionWriter(problemModel.InputFileName, theAlgorithm.GetOutputSummary(), theSolution.GetOutputSummary(), theSolution.GetWritableSolution());
+                writer.Write();
+                theAlgorithm.Reset();
+            }
+        }
+
+        void CreateEMHProblem()
+        {
+            theProblem = new EMH_Problem();
+            AddProblems();
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_NUM_EV, 6);
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_NUM_GDV, 0);
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_LAMBDA, 1);
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_USE_EXACTLY_NUM_EV_AVAILABLE, false);
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_USE_EXACTLY_NUM_GDV_AVAILABLE, false);
+            theProblemModel = new EMH_ProblemModel();
+        }
+        void CreateYCProblem()
+        {
+            theProblem = new EVvsGDV_MinCost_VRP();
+            AddProblems();
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_NUM_EV, 7);
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_NUM_GDV, 7);
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_LAMBDA, 1);
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_USE_EXACTLY_NUM_EV_AVAILABLE, false);
+            theProblem.ProblemCharacteristics.UpdateParameter(Models.ParameterID.PRB_USE_EXACTLY_NUM_GDV_AVAILABLE, false);
+        }
+        void CreateXcplexFormulation()
+        {
+            theAlgorithm = new Outsource2Cplex();
+            theAlgorithm.AlgorithmParameters.UpdateParameter(Models.ParameterID.ALG_EXPORT_LP_MODEL, false);
+            theAlgorithm.AlgorithmParameters.UpdateParameter(Models.ParameterID.ALG_LOG_OUTPUT_TYPE, false);
+            theAlgorithm.AlgorithmParameters.UpdateParameter(Models.ParameterID.ALG_RELAXATION, XCPlexRelaxation.None);
+            theAlgorithm.AlgorithmParameters.UpdateParameter(Models.ParameterID.ALG_RUNTIME_SECONDS, 30.0);
+        }
+        private void AddProblems()
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                RestoreDirectory = true,
+                Multiselect = true
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    for (int i = 0; i < dialog.FileNames.Length; i++)
+                    {
+                        theProblem = ProblemUtil.CreateProblemByFileName(theProblem.GetName(), dialog.FileNames[i]);
+                        problems.Add(theProblem);
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("There is something wrong while parsing the file!", "File parse error!");
+                }
+            }
+
+        }
+        private void CreateProblemModels()
+        {
+            try
+            {
+                for (int i = 0; i < problems.Count; i++)
+                {
+                    problems[i].ProblemCharacteristics.UpdateParameters(theProblem.ProblemCharacteristics);
+                    TSPModelType = typeof(Models.XCPlex.XCPlex_ArcDuplicatingFormulation_woU);
+                    theProblemModel = ProblemModelUtil.CreateProblemModelByProblem(theProblemModel.GetType(), problems[i], TSPModelType);
+                    problemModels.Add(theProblemModel);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There is something wrong while loading problem from the whole data!", "Problem loading error!");
+            }
         }
     }
 }
